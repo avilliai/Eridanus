6d4624eab1170058b3e52ca3802b652081cb91f8
avilliai <1840094972@qq.com>
2024-12-24 01:34:46 +0800

    搓sdk

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ async def handle_group_message(event: GroupMessageEvent):
     print(f"收到群组消息: {event.raw_message}，来自群: {event.group_id}")
     #await bot.send_friend_message(1840094972, "你好，我是机器人")
     await bot.send(event,"我测你的码")
-    # await bot.groupList() 还需要进一步修改，需要使用echo参数判断专有返回值，目前还没有做
+    # await bot.groupList() 还需要进一步修改，需要使用echo参数作为特定请求的返回值标识，目前还没有做
 
 bot.run()
 
