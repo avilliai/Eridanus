b3cd1233750ca687fb890580c3fa9b9965e79955
avilliai <1840094972@qq.com>
2024-12-24 22:11:44 +0800

    沟槽的ws，不用了，用http

---
+++ run/example.py
@@ -0,0 +1,55 @@
+from EridanusTools.event.events import GroupMessageEvent, FriendRequestEvent, PrivateMessageEvent
+from EridanusTools.message.message_components import Record, Node, Text
+
+
+def main(bot):
+    global avatar
+    avatar=False
+    @bot.on(GroupMessageEvent)
+    async def sendLike(event: GroupMessageEvent):
+        if event.raw_message=="赞我":
+            await bot.send_like(event.user_id)
+            await bot.send(event, "已赞你！")
+        if event.raw_message.startswith("叫我"):
+            await bot.send(event, "已修改")
+            remark = event.raw_message.split("叫我")[1].strip()
+            await bot.set_friend_remark(event.user_id, remark)
+
+    @bot.on(FriendRequestEvent)
+    async def FriendRequestHandler(event: FriendRequestEvent):
+        print(event)
+
+    @bot.on(GroupMessageEvent)
+    async def changeAvatar(event: GroupMessageEvent):
+        global avatar
+        #bot.logger.error(event.processed_message)
+        #bot.logger.error(event.get("image"))
+        if event.raw_message=="换头像" and event.sender.user_id==1840094972:
+            await bot.send(event,"发来！")
+            avatar=True
+        if event.get("image") and avatar and event.sender.user_id==1840094972:
+            bot.logger.error(event.get("image")[0]["url"])
+            r=await bot.set_qq_avatar(event.get("image")[0]["url"])
+            bot.logger.error(r)
+            await bot.send(event,"已更换头像！")
+            avatar=False
+        if event.raw_message=="给我管理" and event.sender.user_id==1840094972:
+            await bot.set_group_admin(event.group_id,event.sender.user_id,True)
+            await bot.send(event, "给你了！")
+        if event.raw_message=="取消管理" and event.sender.user_id==1840094972:
+            await bot.set_group_admin(event.group_id,event.sender.user_id,False)
+            await bot.send(event, "取消了！")
+        if event.raw_message.startswith("我要头衔"):
+            title=event.raw_message.split("我要头衔")[1].strip()
+            await bot.set_group_special_title(event.group_id,event.sender.user_id,title)
+            await bot.send(event, "已设置头衔！")
+        if event.raw_message=="禁言我":
+            await bot.mute(event.group_id,event.sender.user_id,60)
+        if event.raw_message=="测试":
+            r=Node(content=[Text("你好，我是机器人！")])
+            await bot.send_group_forward_msg(event.group_id,r)
+            await bot.send(event,Record(file="file://D:/python/Manyana/data/autoReply/voiceReply/a1axataxaWaQaia.wav"))
+    @bot.on(PrivateMessageEvent)
+    async def FriendMesHandler(event: PrivateMessageEvent):
+        if event.raw_message=="戳我":
+            await bot.friend_poke(event.sender.user_id)
---
+++ test2.py
@@ -0,0 +1,40 @@
+import json
+from asyncio import sleep
+
+from EridanusTools.event.events import GroupMessageEvent, PrivateMessageEvent, GroupRecallNoticeEvent
+from EridanusTools.message.message_components import Image
+from EridanusTools.AdapterAndBot import HTTPBot
+from run import example
+
+bot = HTTPBot(http_sever="http://127.0.0.1:3000",access_token="any_access_token")
+
+
+@bot.on(PrivateMessageEvent)
+async def handle_private_message(event: PrivateMessageEvent):
+    print(f"收到私聊消息: {event.raw_message} 来自用户: {event.sender.nickname}")
+
+    #await bot.send_friend_message(event.sender.user_id, "你好，我测你的码")
+
+
+@bot.on(GroupMessageEvent)
+async def handle_group_message(event: GroupMessageEvent):
+    #print(event)
+    print(f"收到群组消息: {event.processed_message}，来自群: {event.group_id}")
+    #await bot.send_friend_message(1840094972, "你好，我是机器人")
+    #r=await bot.send(event,["我测你的码",Image(file="file://D:\python\Eridanus\data\pictures\ccc.png")])
+    #r=await bot.get_friend_list()
+    #print(r)
+    #await bot.recall(r["data"]["message_id"])
+    #r=await bot.groupList() #还需要进一步修改，需要使用echo参数作为特定请求的返回值标识，目前还没有做
+    #r=await bot.query_echo(r)
+    #print(r)
+
+    #print(r)
+@bot.on(GroupRecallNoticeEvent)
+async def handle_group_recall_notice(event: GroupRecallNoticeEvent):
+    print(f"群组消息撤回: {event.message_id}，来自群: {event.group_id}")
+    await bot.send_group_message(event.group_id, "撤回牛魔")
+example.main(bot)
+
+bot.run(host="0.0.0.0", port= 8080)
+
