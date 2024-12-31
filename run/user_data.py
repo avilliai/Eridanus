245e1bd1bfe11f86fcd236404d2fb8d12f6c5b6a
avilliai <1840094972@qq.com>
2025-01-01 00:55:33 +0800

    asmr阻塞问题修复

--- run/user_data.py
+++ run/user_data.py
@@ -79,11 +79,14 @@ def main(bot,config):
     async def handle_group_message(event):
         if config.controller["user_data"]["auto_register"]:
             data = await bot.get_group_member_info(group_id=event.group_id, user_id=event.user_id)
-            await add_user(
-                data["data"]["user_id"],
-                data["data"]["nickname"],
-                data["data"]["card"],
-                data["data"]["sex"],
-                data["data"]["age"],
-                data["data"]["area"])
+            try:
+                await add_user(
+                    data["data"]["user_id"],
+                    data["data"]["nickname"],
+                    data["data"]["card"],
+                    data["data"]["sex"],
+                    data["data"]["age"],
+                    data["data"]["area"])
+            except:
+                pass #我管你这那的....
             #await bot.send(event, r)
