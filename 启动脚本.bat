2d2a6e9760742045857c459b199edfa63733ed9c
avilliai <1840094972@qq.com>
2025-01-09 21:30:09 +0800

    还是等整合包吧 pip install 产生的报错一般用户根本没法解决。

--- 启动脚本.bat
+++ 启动脚本.bat
@@ -1,7 +1,14 @@
+@echo off
+set PYTHON_EXE=..\environments\Python311\python.exe
+set MAIN_SCRIPT=main.py
 
-cd venv\Scripts
-call activate.bat
-cd ..\..
-python main.py
-
+rem checking.....
+if exist "%PYTHON_EXE%" (
+    "%PYTHON_EXE%" "%MAIN_SCRIPT%"
+) else (
+    cd venv\Scripts
+    call activate.bat
+    cd ..\..
+    python %MAIN_SCRIPT%
+)
 pause
