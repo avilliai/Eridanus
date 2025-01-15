5126232b55b7621d995f25afb999d1b58173a4b6
avilliai <1840094972@qq.com>
2025-01-15 07:11:05 +0000

    fix bug

--- run/groupManager/group_manager.py
+++ run/groupManager/group_manager.py
@@ -4,5 +4,6 @@ from developTools.event.events import GroupDecreaseNoticeEvent
 def main(bot,config):
 
     @bot.on(GroupDecreaseNoticeEvent)
-    async def group_decrease(event):
-        await bot.send_group_message(event.group_id, f"{event.user_id} 悄悄离开了")
+    async def group_decrease(event: GroupDecreaseNoticeEvent):
+        if event.user_id != event.self_id:
+            await bot.send_group_message(event.group_id, f"{event.user_id} 悄悄离开了")
