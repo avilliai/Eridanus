f17ab49d307e07a167d79928abd764c43369de3e
avilliai <1840094972@qq.com>
2025-02-06 10:44:21 +0800

    联网搜索

--- plugins/func_map.py
+++ plugins/func_map.py
@@ -1,6 +1,18 @@
 #为func_calling提供函数映射
 import inspect
+import json
 
+from run.basic_plugin import call_weather_query,call_setu,call_image_search,call_tts,call_tarot,call_pick_music,call_fortune,call_all_speakers
+from run.user_data import call_user_data_register,call_user_data_query,call_user_data_sign,call_change_city,call_change_name,call_permit
+from run.groupManager.self_Manager import call_operate_blandwhite,garbage_collection,report_to_master,send,send_contract
+from run.groupManager.nailong_get import operate_group_censor
+from run.resource_search.resource_search import search_book_info,call_asmr,call_download_book
+from run.user_data import call_delete_user_history,call_clear_all_history
+from run.acg_infromation.bangumi import call_bangumi_search
+from run.streaming_media.youtube import download_youtube
+from run.aiDraw import call_text2img,call_aiArtModerate
+from run.scheduledTasks import operate_group_push_tasks
+from run.resource_search.engine_search import search_net
 
 async def call_quit_chat(bot,event,config):
     return False
