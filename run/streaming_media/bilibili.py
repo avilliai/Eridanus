04d1e410015912517a8854e2eecc30950231a671
avilliai <1840094972@qq.com>
2025-02-09 22:05:31 +0800

    error

--- run/streaming_media/bilibili.py
+++ run/streaming_media/bilibili.py
@@ -33,11 +33,18 @@ async def check_bili_dynamic(bot,config):
                 groups=config.bili_dynamic[target_uid]["push_groups"]
                 try:
                     if bilibili_type_draw == 1:
-                        dynamic = await fetch_dynamic(latest_dynamic_id,config.settings["bili_dynamic"]["screen_shot_mode"])
+                        try:
+                            dynamic = await fetch_dynamic(latest_dynamic_id,config.settings["bili_dynamic"]["screen_shot_mode"])
+                        except:
+                            bilibili_type_draw = 2
                     elif bilibili_type_draw == 2:
-                        linking_prising_json=await link_prising(f'https://t.bilibili.com/{latest_dynamic_id}', filepath='data/pictures/cache/',type = 'dynamic_check')
-                        if not linking_prising_json['status']:continue
-                        dynamic= linking_prising_json['pic_path']
+                        try:
+                            linking_prising_json=await link_prising(f'https://t.bilibili.com/{latest_dynamic_id}', filepath='data/pictures/cache/',type = 'dynamic_check')
+                            if not linking_prising_json['status']:continue
+                            dynamic= linking_prising_json['pic_path']
+                        except:
+                            dynamic = await fetch_dynamic(latest_dynamic_id,
+                                                          config.settings["bili_dynamic"]["screen_shot_mode"])
                 except Exception as e:
                     bot.logger.error(f"动态获取失败 ：{e} 关注id: {target_uid} 最新动态id: {latest_dynamic_id}")
                     continue
