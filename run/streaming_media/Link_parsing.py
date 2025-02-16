8350f06ab722e69c2d4f79e5fa9a1ad14e5283d4
QcrTiMo <3192377376@qq.com>
2025-02-16 22:13:20 +0800

    Update Link_parsing.py

--- run/streaming_media/Link_parsing.py
+++ run/streaming_media/Link_parsing.py
@@ -88,16 +88,17 @@ def main(bot,config):
 
     @bot.on(GroupMessageEvent)
     async def Music_Link_Prising_search(event: GroupMessageEvent):
-        proxy = config.api["proxy"]["http_proxy"]
-        url = event.raw_message
-        if "music.163.com" not in url:
-            return
-        link_parsing_json = await netease_music_link_parse(url, filepath='data/pictures/cache/')
-        if link_parsing_json['status']:
-            bot.logger.info('网易云音乐链接解析成功，开始推送~~~')
-            await bot.send(event, Image(file=link_parsing_json['pic_path']))
-        else:
-            if link_parsing_json['reason']:
-                bot.logger.error(f'netease_music_link_error: {link_parsing_json["reason"]}')
+        if config.settings["网易云卡片"]["enable"]:
+            proxy = config.api["proxy"]["http_proxy"]
+            url = event.raw_message
+            if "music.163.com" not in url:
+                return
+            link_parsing_json = await netease_music_link_parse(url, filepath='data/pictures/cache/')
+            if link_parsing_json['status']:
+                bot.logger.info('网易云音乐链接解析成功，开始推送~~~')
+                await bot.send(event, Image(file=link_parsing_json['pic_path']))
+            else:
+                if link_parsing_json['reason']:
+                    bot.logger.error(f'netease_music_link_error: {link_parsing_json["reason"]}')
 
 
