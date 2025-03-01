966c0cd083984c25eebfd411f50a6ab42ea4f0f6
avilliai <1840094972@qq.com>
2025-03-01 19:18:41 +0800

    插件加载机制重写

--- run/system_plugin/__init__.py
+++ run/system_plugin/__init__.py
@@ -1,5 +1,6 @@
 dynamic_imports = {
     "run.system_plugin.func_collection": ["operate_group_push_tasks"],
+    "run.system_plugin.Mface_Record": ["call_send_mface"]
 }
 function_declarations=[
     {
