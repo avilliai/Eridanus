3ce9e2fbf7c5ce5b95cc99381de23e064efa6c28
avilliai <1840094972@qq.com>
2025-05-18 20:14:36 +0800

    新增指令，随机忍术

--- run/group_fun/ninjutsu.py
+++ run/group_fun/ninjutsu.py
@@ -2,12 +2,12 @@
 from developTools.event.events import GroupMessageEvent
 from framework_common.framework_util.websocket_fix import ExtendBot
 from framework_common.framework_util.yamlLoader import YAMLManager
+from run.group_fun.func_collection import random_ninjutsu
 from run.group_fun.service.lex_burner_Ninja import Lexburner_Ninja
 
 
 def main(bot: ExtendBot,config: YAMLManager):
-    ninja=Lexburner_Ninja()
     @bot.on(GroupMessageEvent)
     async def handle_group_message(event: GroupMessageEvent):
         if event.pure_text=="随机忍术":
-            await bot.send(event,"Hello World!")
+            await random_ninjutsu(bot,event,config)
