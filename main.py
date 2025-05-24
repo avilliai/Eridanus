c598cce0dae372e2343aef2bcedaf17f1cec0dba
avilliai <1840094972@qq.com>
2025-05-24 18:26:27 +0800

    info改warning

--- main.py
+++ main.py
@@ -51,8 +51,6 @@ if config.common_config.basic_config["webui"]["enable"]:
     bot1.logger.info("主线程：WebUI 已启动在子线程中")
 
 
-
-
 PLUGIN_DIR = "run"
 # 创建模块缓存字典
 module_cache = {}
@@ -97,7 +95,7 @@ def find_plugins(plugin_dir=PLUGIN_DIR):
                     plugin_modules.append((plugin_name, module_name, module))
                 else:
                     if plugin_name != "nailong_get" and plugin_name != "func_collection" and f"service" not in module_name:
-                        bot1.logger.info(
+                        bot1.logger.warning(
                             f"⚠️ The plugin `{module_path} {plugin_name}` does not have a main() method. If this plugin is a function collection, please ignore this warning.")
 
     return plugin_modules
