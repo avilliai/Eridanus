280b36fdfe66b4549d55bf557213a54c9994ff40
avilliai <1840094972@qq.com>
2025-06-01 18:28:02 +0800

    兼容性修复

--- developTools/message/message_components.py
+++ developTools/message/message_components.py
@@ -224,7 +224,7 @@ class Forward(MessageComponent):
 class Node(MessageComponent):
     comp_type: str = "node"
     user_id: str = Field(default="3552663628",description="发送者 QQ 号")
-    nickname: str = Field(default="onebot",description="发送者昵称")
+    nickname: str = Field(default="Eridanus",description="发送者昵称")
     content: str | list[MessageComponent] = Field(description="消息内容")
 
 
